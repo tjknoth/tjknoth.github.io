bc7f5b22083b4d24402c595c48c03cc64be74303
Tristan Knoth <tjknoth@gmail.com>
2023-07-24 16:30:56 -0700

    update versions

--- src/site.hs
+++ src/site.hs
@@ -7,16 +7,6 @@ import           Text.Pandoc
 import           Text.Pandoc.Options
 import           Text.Pandoc.Walk (walk)
 
-
-crunchWithCtxCustom mode ctx = do
-  route $ setExtension "html"
-  compile $ pandocCompilerWithTransform
-              defaultHakyllReaderOptions
-              defaultHakyllWriterOptions
-              (walk (toggleMode mode . haskellizeBlock) . walk haskellizeInline)
-          >>= loadAndApplyTemplate "templates/course.html" ctx
-          >>= relativizeUrls
-
 main :: IO ()
 main = do
   deploy <- readFile "src/deployScript"
@@ -99,26 +89,3 @@ pandocMathCompiler =
                       writerHTMLMathMethod = MathJax ""
                     }
   in pandocCompilerWith defaultHakyllReaderOptions writerOpts
-
---------------------------------------------------------------------------------
-
--- | Treat an ordered list with uppercase roman numerals as a map:
---   in each item, the first paragraph is the key, and the second is the value;
---   pick the value with the key `mode` and discard all other items
-toggleMode :: String -> Block -> Block
-toggleMode mode (OrderedList (_, UpperRoman, _) items) = select items
-  where
-    select ([Para [Str key], payload] : rest) =
-      if key == mode then payload else select rest
-    select _ = Null
-toggleMode _ b = b
-
-haskellizeInline :: Inline -> Inline
-haskellizeInline (Code (ident, [], kvs) str) = Code (ident, ["haskell"], kvs) str
-haskellizeInline b = b
-
-haskellizeBlock :: Block -> Block
-haskellizeBlock (CodeBlock (ident, [], kvs) str) = CodeBlock (ident, ["haskell"], kvs) str
-haskellizeBlock b = b
-
-
